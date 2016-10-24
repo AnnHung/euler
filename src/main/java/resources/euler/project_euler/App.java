98ce94bb41a3704fe12767abc50660774a178b98
Nam Nguyễn Phương <Nam Nguyn Phng>
2016-10-24 23:27:51 +0700

    init commit

---
+++ src/main/java/resources/euler/project_euler/App.java
@@ -0,0 +1,13 @@
+package resources.euler.project_euler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
